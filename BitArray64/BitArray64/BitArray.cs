ebee60bb5370a0116efa59714298bbffeb0b1ee4
Adelina Ciorap <c.adelina@outlook.com>
2019-06-07 11:57:31 +0300

    completed bitaaray

---
+++ BitArray64/BitArray64/BitArray.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitArray64
+{
+    class BitArray: IEnumerable<int>
+        
+    {
+        private ulong number;
+        private int[]bitArray= new int[64];
+
+        public BitArray(ulong number)
+        {
+            this.Number = number;
+            for (int i=0; i<16; i++)
+            {
+                bitArray[i] = (int)number % 2;
+                number /= 2;
+            }
+        }
+
+            public ulong Number { get; set; }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for(int i=63; i >= 0; i--)
+            {
+                yield return this.bitArray[i];
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            BitArray bitArray = obj as BitArray;
+            if ((object)bitArray == null)
+            {
+                return false;
+            }
+
+            else if(object.Equals(this.number, bitArray.number))
+                {
+                return true;
+                }
+            return false;
+        }
+
+        public static bool operator == (BitArray first, BitArray second)
+        {
+            return BitArray.Equals(first, second);
+        }
+         public static bool operator !=(BitArray first, BitArray second)
+        {
+            return BitArray.Equals(first, second);
+        }
+
+        public int this[int index]
+        {
+            get { return this.bitArray[index]; }
+            set
+            {
+                if(value==1 || value == 0)
+                {
+                    this.bitArray[index] = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("The value must be [1] or [0].");
+                }
+
+
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return this.number.GetHashCode() ^ this.bitArray.GetHashCode();
+        }
+
+        
+    }
+}
---
+++ BitArray64/BitArray64/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitArray64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BitArray number = new BitArray(34);
+            foreach (var bit in number)
+            {
+                Console.Write(bit);
+            }
+            Console.WriteLine();
+
+            BitArray number2 = new BitArray(23);
+            foreach (var bit in number2)
+            {
+                Console.Write(bit);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine(number.Equals(number2));
+            Console.WriteLine(number!=number2);
+            Console.WriteLine(number[2]);
+
+            Console.WriteLine(number.GetHashCode());
+           
+
+        }
+    }
+}
